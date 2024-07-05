d2e0f3dd8f2cf62eb975db0b9e1d7e6f9491edd2
cpkopp <cameronpkopp@gmail.com>
2024-07-05 10:00:29 -1000

    initial commit

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using EmployeeManagement;
+
+namespace EmployeeManagement.Models
+{
+  public class Employee
+  {
+    public int Id {get; set;}
+    public string FirstName {get; set;}
+    public string LastName {get; set;}
+    public string Email {get; set;}
+    public string Phone {get; set;}
+    public string Position {get; set;}
+  }
+}
