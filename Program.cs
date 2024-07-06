9f857a33235d0267a2ec0ed4c5c24d10a8b6a5ec
cpkopp <cameronpkopp@gmail.com>
2024-07-06 09:04:37 -1000

    entity framework in memory db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+
 namespace EmployeeManagement 
 {
   public class Program
   {
     public static void Main(string[] args) {
       var builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddDbContext<AppDbContext>(
+        options => options.UseInMemoryDatabase("EmployeeDb")
+      );
+
       var app = builder.Build();
 
       app.MapGet("/", () => "Hello World!");
